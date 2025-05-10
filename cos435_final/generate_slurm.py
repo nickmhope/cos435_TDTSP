f62c7b8bdc9d432a84d9b23c1987e9b4ca47ab1f
Ryan Hoffman <rh8490@princeton.edu>
2025-05-10 19:58:53 -0400

    less cores per job

--- cos435_final/generate_slurm.py
+++ cos435_final/generate_slurm.py
@@ -4,7 +4,7 @@ def generate_slurm_script(N, num_iters, train_steps, benchmark_size):
 #SBATCH --job-name=eval_N={N}_iters={num_iters}   # create a short name for your job
 #SBATCH --nodes=1                # node count
 #SBATCH --ntasks=1               # total number of tasks across all nodes
-#SBATCH --cpus-per-task=32        # cpu-cores per task (>1 if multi-threaded tasks)
+#SBATCH --cpus-per-task=16        # cpu-cores per task (>1 if multi-threaded tasks)
 #SBATCH --mem-per-cpu=1G         # memory per cpu-core (4G is default)
 #SBATCH --time=24:00:00          # total run time limit (HH:MM:SS)
 #SBATCH --mail-type=begin        # send email when job begins
