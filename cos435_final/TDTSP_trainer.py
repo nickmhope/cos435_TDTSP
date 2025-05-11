11adc6824329b27ec668c96d47746da02a543ef2
Ryan Hoffman <rh8490@princeton.edu>
2025-05-10 21:48:28 -0400

    changed reward in tdtsp eval_func

--- cos435_final/TDTSP_trainer.py
+++ cos435_final/TDTSP_trainer.py
@@ -8,8 +8,6 @@ import matplotlib.pyplot as plt
 import argparse
 import pickle
 import pandas as pd
-from python_tsp.exact import solve_tsp_dynamic_programming
-from python_tsp.heuristics import solve_tsp_simulated_annealing
 
 # define helper function for model evaluation
 def eval_func(model):
@@ -34,101 +32,10 @@ def eval_func(model):
             total_reward += reward
             if truncated and not done:
                 print(f'WARNING: TRUNCATION on benchmark example {i}')
-        total_time += eval_env.current_time / benchmark_size
+        total_time += (eval_env.current_time - start_time_set[i]) / benchmark_size
         avg_reward = total_reward / benchmark_size
 
     return total_time, avg_reward
-
-def discretize_poly_matrix(poly_matrix, N):
-    discretization_constant = 1000
-    tensor = np.zeros((N, N, discretization_constant), dtype=np.float32)
-    for i in range(N):
-        for j in range(N):
-            for k in range(discretization_constant):
-                t = k * 0.024
-                tensor[i, j, k] = poly_matrix[i, j].eval(t)
-    return tensor
-
-def nearest_neighbors_with_time(travel_tensor, start_node, visit_nodes):
-    visit_nodes = visit_nodes.copy()
-    path = [start_node]
-    current_node = start_node
-    current_time = 0
-    total_time = 0
-
-    while len(visit_nodes) > 0:
-        best_node = None
-        best_time = float('inf')
-        for node in visit_nodes:
-            time_index = min(int(current_time), travel_tensor.shape[2] - 1)
-            travel_time = travel_tensor[current_node, node, time_index]
-            if travel_time < best_time:
-                best_time = travel_time
-                best_node = node
-
-        if best_node is None:
-            break
-
-        # Update current state
-        visit_nodes.remove(best_node)
-        current_time += best_time
-        total_time += best_time
-        path.append(best_node)
-        current_node = best_node
-
-    return path, total_time
-
-def tsp_solver_dynamic_programming(travel_tensor, destinations):
-    tensor = travel_tensor[:,:,:60].mean(axis=2).copy()
-    keep = destinations.astype(bool)
-    distance_matrix = tensor[keep][:, keep]
-    permutation, distance = solve_tsp_dynamic_programming(distance_matrix)
-    route = np.where(destinations == 1)[0][permutation]
-    return route, distance
-
-def tsp_heuristic_sim_annealing(travel_tensor, destinations):
-    tensor = travel_tensor[:,:,:60].mean(axis=2).copy()
-    keep = destinations.astype(bool)
-    distance_matrix = tensor[keep][:, keep]
-    permutation, distance = solve_tsp_simulated_annealing(distance_matrix, max_processing_time=180)
-    route = np.where(destinations == 1)[0][permutation]
-    return route, distance
-
-def evaluate_tsp_solver(permutation, poly_matrix):
-    i = permutation[0]
-    t = 0
-    for j in permutation[1:]:
-        travel_time = poly_matrix[i, j].eval(t)
-        t += travel_time
-        i = j
-    return t
-    visit_nodes = visit_nodes.copy()
-    path = [start_node]
-    current_node = start_node
-    current_time = 0
-    total_time = 0
-
-    while len(visit_nodes) > 0:
-        best_node = None
-        best_time = float('inf')
-        for node in visit_nodes:
-            time_index = min(int(current_time), travel_tensor.shape[2] - 1)
-            travel_time = travel_tensor[current_node, node, time_index]
-            if travel_time < best_time:
-                best_time = travel_time
-                best_node = node
-
-        if best_node is None:
-            break
-
-        # Update current state
-        visit_nodes.remove(best_node)
-        current_time += best_time
-        total_time += best_time
-        path.append(best_node)
-        current_node = best_node
-
-    return path, total_time
 
 if __name__ == '__main__':
     # Environment parameters
